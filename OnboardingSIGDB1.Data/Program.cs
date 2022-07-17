01fb165bef9adbc976ab4d68cdd097c6a00f01b6
Lilan Queiroz Thomazini <lilanmat@gmail.com>
2022-07-17 10:46:58 -0300

    Criacao da Estrutura do Projeto

---
+++ OnboardingSIGDB1.Data/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OnboardingSIGDB1.Data
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
